75283fdc8f114707b0cc2feb45a6cbf9bbaf386c
Thales Gregorio <137453849+thales-gsd@users.noreply.github.com>
2026-03-18 12:27:25 -0300

    styling the components and adjusting a few things

--- coming-soon/src/components/EmailField/EmailField.jsx
+++ coming-soon/src/components/EmailField/EmailField.jsx
@@ -5,7 +5,6 @@ export default function EmailField() {
   const [email, setEmail] = useState('');
   const [isInvalid, setIsInvalid] = useState(false);
 
-  // Função para validar o formato do email
   const validateEmail = (email) => {
     return /\S+@\S+\.\S+/.test(email);
   };
@@ -16,29 +15,31 @@ export default function EmailField() {
       setIsInvalid(true);
     } else {
       setIsInvalid(false);
-      console.log("Email enviado com sucesso:", email);
     }
   };
 
   return (
     <form className='email-container' onSubmit={handleSubmit} noValidate>
-      <input 
-        type="email" 
-        className={`email-input ${isInvalid ? 'input-error' : ''}`} 
-        name="email" 
-        id="email" 
-        placeholder='Email Address'
-        value={email}
-        onChange={(e) => {
-          setEmail(e.target.value);
-          if (isInvalid) setIsInvalid(false);
-        }}
-      />
-      {isInvalid && <img className='error-icon' src='/icon-error.svg' alt='error' />}
-      <button type="submit">
-        <img src="/icon-arrow.svg" alt="submit" />
-      </button>
-      {isInvalid && <p className='error-mensage'>Please provide a valid email</p>}
+      <div className={`email-field ${isInvalid ? 'field-error' : ''}`}>
+        <input 
+          type="email" 
+          className="email-input" 
+          placeholder='Email Address'
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+            if (isInvalid) setIsInvalid(false);
+          }}
+        />
+        
+        {isInvalid && <img className='error-icon' src='/icon-error.svg' alt='' />}
+        
+        <button type="submit" className="submit-button">
+          <img src="/icon-arrow.svg" alt="submit" />
+        </button>
+      </div>
+      
+      {isInvalid && <p className='error-message'>Please provide a valid email</p>}
     </form>
   );
 };
